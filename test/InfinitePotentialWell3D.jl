19e869a1cc33472e894430173aa72a3ead50a341
lhapp27 <lucas.happ@riken.jp>
2024-04-24 15:30:13 +0900

    Add tests for PoschlTeller and InfinitePotentialWell3D

--- test/InfinitePotentialWell3D.jl
+++ test/InfinitePotentialWell3D.jl
@@ -44,3 +44,92 @@ end
 
 println("""```
 """)
+
+
+
+# <ψₙ|H|ψₙ>  = ∫ψₙ*Tψₙdx = Eₙ
+
+
+println(raw"""
+#### Eigen Values
+
+```math
+  \begin{aligned}
+    E_n
+    &=      \int_0^L \psi^\ast_n(x) \hat{H} \psi_n(x) ~\mathrm{d}x \\
+    &=      \int_0^L \psi^\ast_n(x) \left[ \hat{V} + \hat{T} \right] \psi(x) ~\mathrm{d}x \\
+    &=      \int_0^L \psi^\ast_n(x) \left[ 0 - \frac{\hbar^2}{2m} \frac{\mathrm{d}^{2}}{\mathrm{d} x^{2}} \right] \psi(x) ~\mathrm{d}x \\
+    &\simeq \int_0^L \psi^\ast_n(x) \left[ -\frac{\hbar^2}{2m} \frac{\psi(x+\Delta x) - 2\psi(x) + \psi(x-\Delta x)}{\Delta x^{2}} \right] ~\mathrm{d}x.
+  \end{aligned}
+```
+
+Where, the difference formula for the 2nd-order derivative:
+
+```math
+\begin{aligned}
+  % 2\psi(x)
+  % + \frac{\mathrm{d}^{2} \psi(x)}{\mathrm{d} x^{2}} \Delta x^{2}
+  % + O\left(\Delta x^{4}\right)
+  % &=
+  % \psi(x+\Delta x)
+  % + \psi(x-\Delta x)
+  % \\
+  % \frac{\mathrm{d}^{2} \psi(x)}{\mathrm{d} x^{2}} \Delta x^{2}
+  % &=
+  % \psi(x+\Delta x)
+  % - 2\psi(x)
+  % + \psi(x-\Delta x)
+  % - O\left(\Delta x^{4}\right)
+  % \\
+  % \frac{\mathrm{d}^{2} \psi(x)}{\mathrm{d} x^{2}}
+  % &=
+  % \frac{\psi(x+\Delta x) - 2\psi(x) + \psi(x-\Delta x)}{\Delta x^{2}}
+  % - \frac{O\left(\Delta x^{4}\right)}{\Delta x^{2}}
+  % \\
+  \frac{\mathrm{d}^{2} \psi(x)}{\mathrm{d} x^{2}}
+  &=
+  \frac{\psi(x+\Delta x) - 2\psi(x) + \psi(x-\Delta x)}{\Delta x^{2}}
+  + O\left(\Delta x^{2}\right)
+\end{aligned}
+```
+
+are given by the sum of 2 Taylor series:
+
+```math
+\begin{aligned}
+\psi(x+\Delta x)
+&= \psi(x)
++ \frac{\mathrm{d} \psi(x)}{\mathrm{d} x} \Delta x
++ \frac{1}{2!} \frac{\mathrm{d}^{2} \psi(x)}{\mathrm{d} x^{2}} \Delta x^{2}
++ \frac{1}{3!} \frac{\mathrm{d}^{3} \psi(x)}{\mathrm{d} x^{3}} \Delta x^{3}
++ O\left(\Delta x^{4}\right),
+\\
+\psi(x-\Delta x)
+&= \psi(x)
+- \frac{\mathrm{d} \psi(x)}{\mathrm{d} x} \Delta x
++ \frac{1}{2!} \frac{\mathrm{d}^{2} \psi(x)}{\mathrm{d} x^{2}} \Delta x^{2}
+- \frac{1}{3!} \frac{\mathrm{d}^{3} \psi(x)}{\mathrm{d} x^{3}} \Delta x^{3}
++ O\left(\Delta x^{4}\right).
+\end{aligned}
+```
+```""")
+
+@testset "<ψₙ|H|ψₙ>  = ∫ψₙ*Tψₙdx = Eₙ" begin
+  ψTψ(IPW3D, x,y,z; nx=0, ny=0,nz=0, Δx=0.01,Δy=0.01,Δz=0.01) = -IPW3D.ℏ^2/(2*IPW3D.m)*conj(ψ(IPW3D,x,y,z,nx=nx,ny=ny,nz=nz))*((ψ(IPW3D,x+Δx,y,z,nx=nx,ny=ny,nz=nz)-2*ψ(IPW3D,x,y,z,nx=nx,ny=ny,nz=nz)+ψ(IPW3D,x-Δx,y,z,nx=nx,ny=ny,nz=nz))/Δx^2 + (ψ(IPW3D,x,y+Δy,z,nx=nx,ny=ny,nz=nz)-2*ψ(IPW3D,x,y,z,nx=nx,ny=ny,nz=nz)+ψ(IPW3D,x,y-Δy,z,nx=nx,ny=ny,nz=nz))/Δy^2 + (ψ(IPW3D,x,y,z+Δz,nx=nx,ny=ny,nz=nz)-2*ψ(IPW3D,x,y,z,nx=nx,ny=ny,nz=nz)+ψ(IPW3D,x,y,z-Δz,nx=nx,ny=ny,nz=nz))/Δz^2)
+  println(" nx |  ny |  nz |        analytical |         numerical ")
+  println(" -- | --- | --- | ----------------- | ----------------- ")
+  for nx in [1,2]
+  for ny in [1,2]
+  for nz in [1,2]
+    IPW3D = InfinitePotentialWell3D(Lx=1.0,Ly=2.0,Lz=3.0)
+    analytical = E(IPW3D,nx=nx,ny=ny,nz=nz)
+    numerical  = quadgk(x -> quadgk(y-> quadgk(z-> ψTψ(IPW3D, x,y,z,nx=nx,ny=ny,nz=nz, Δx=IPW3D.Lx*0.0001, Δy=IPW3D.Ly*0.0001, Δz=IPW3D.Lz*0.0001), 0, IPW3D.Lz, atol=10^-5)[1], 0, IPW3D.Ly, atol=10^-5)[1], 0, IPW3D.Lz, atol=10^-5)[1]
+    acceptance = iszero(analytical) ? isapprox(analytical, numerical, atol=1e-4) : isapprox(analytical, numerical, rtol=1e-5)
+    @test acceptance
+    @printf(" %2d | %3d | %3d | %17.12f | %17.12f %s\n", nx, ny, nz, numerical, analytical, acceptance ? "✔" :  "✗")
+  end
+  end
+  end
+end
+
+println("""```""")
