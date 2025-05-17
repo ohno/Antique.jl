822293ec81d939e2c7d53a032bbae16c7a9d7109
Shuhei Ohno <ohno.github@gmail.com>
2025-05-17 18:47:38 +0900

    Fix dev/docs.jl #53

--- dev/docs.jl
+++ dev/docs.jl
@@ -1,3 +1,7 @@
 # Please run `include("./dev/docs.jl")` on RELP.
 
-run(`julia --project=docs/ -e 'using Pkg; Pkg.activate("./"); cd("docs"); include("make.jl")'`)
+run(`julia --project=docs/ -e 'cd("docs"); using Pkg; Pkg.activate("./"); include("make.jl")'`)
+
+# Please try fowlowing command after remove Manifest.toml if you have any error.
+
+# run(`julia --project=docs/ -e 'cd("docs"); using Pkg; Pkg.activate("./"); Pkg.instantiate()'`)
