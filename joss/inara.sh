86ce40aa590fba1974cbec9c12fde622c455455f
Shuhei Ohno <ohno.github@gmail.com>
2025-05-27 22:26:15 +0900

    Update .gitignore

--- joss/inara.sh
+++ joss/inara.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://github.com/openjournals/inara
 # Boot "Docker Desktop" before running this script.
-# Run `bash make.sh` on pwsh or WSL.
+# Run `bash inara.sh` on pwsh or WSL.
 docker run --rm -it \
     -v $PWD:/data \
     -u $(id -u):$(id -g) \
