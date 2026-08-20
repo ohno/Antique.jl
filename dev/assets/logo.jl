bd3292b2239c7abdba2de7497a6df31e056979ad
Documenter.jl <documenter@juliadocs.github.io>
2026-08-20 17:45:04 +0000

    build based on ede78d8

--- dev/assets/logo.jl
+++ dev/assets/logo.jl
@@ -10,9 +10,9 @@ using Pkg
 Pkg.activate(dir)
 using Antique
 
-function XY2path(X,Y)
+function XY2path(X, Y)
     X = 252 / (maximum(X) - minimum(X)) * (X .- minimum(X)) .+ 50
-    Y = 260 .- 445  .*  Y
+    Y = 260 .- 445 .* Y
     path = "M$(X[1]),$(Y[1]) "
     for i in 2:length(X)
         path *= "L$(X[i]),$(Y[i]) "
@@ -20,17 +20,17 @@ function XY2path(X,Y)
     return path
 end
 
-HA = HydrogenAtom(Z=1, E_h=1.0, a_0=1.0, m_e=1.0, hbar=1.0)
+HA = HydrogenAtom(Z = 1, E_h = 1.0, a_0 = 1.0, m_e = 1.0, hbar = 1.0)
 paths = Dict()
 X = 0.0:0.01:8
-Y1 = [x^2 * Antique.radial_function(HA,x,n=1,l=0)^2 for x in X]
-Y2 = [x^2 * Antique.radial_function(HA,x,n=2,l=0)^2 for x in X]
-Y3 = [x^2 * Antique.radial_function(HA,x,n=2,l=1)^2 for x in X]
-Y4 = [x^2 * Antique.radial_function(HA,x,n=3,l=0)^2 for x in X]
-paths[1] = XY2path(X,Y1)
-paths[2] = XY2path(X,Y2)
-paths[3] = XY2path(X,Y3)
-paths[4] = XY2path(X,Y4)
+Y1 = [x^2 * Antique.radial_function(HA, x, n = 1, l = 0)^2 for x in X]
+Y2 = [x^2 * Antique.radial_function(HA, x, n = 2, l = 0)^2 for x in X]
+Y3 = [x^2 * Antique.radial_function(HA, x, n = 2, l = 1)^2 for x in X]
+Y4 = [x^2 * Antique.radial_function(HA, x, n = 3, l = 0)^2 for x in X]
+paths[1] = XY2path(X, Y1)
+paths[2] = XY2path(X, Y2)
+paths[3] = XY2path(X, Y3)
+paths[4] = XY2path(X, Y4)
 
 colors = ["#4063D8", "#CB3C33", "#389826", "#9558B2"]
 
@@ -50,8 +50,8 @@ svg = """
 
   <!-- Defs -->
   <defs>
-    <marker id="arrow" markerWidth="$(as)" markerHeight="$(as)" refX="0" refY="$(as/2)" orient="auto">
-      <polygon points="0,0 0,$(as) $(as),$(as/2)" fill="#24292E"/>
+    <marker id="arrow" markerWidth="$(as)" markerHeight="$(as)" refX="0" refY="$(as / 2)" orient="auto">
+      <polygon points="0,0 0,$(as) $(as),$(as / 2)" fill="#24292E"/>
     </marker>
   </defs>
 
